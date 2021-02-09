79d1c81d4ccef928aa8a671b5aeb28a0c3c2b7ea
Maryna Skrypnyk <marinaskripnik97@gmail.com>
2021-02-09 15:49:02 +0200

    terms-conditions and refund-policy pages

--- resources/js/views/RefundPolicy.vue
+++ resources/js/views/RefundPolicy.vue
@@ -1,13 +1,65 @@
 <template>
-
+  <PageLayout background-color="bgColor">
+    <template v-slot:title>
+      {{t('refundPolicy')}}
+    </template>
+    <GradientContainer class="refund" color="bgColor">
+      <h2>{{t('refundPolicy')}}</h2>
+      <p
+        :key="i"
+        v-for="(paragraph, i) in paragraphs"
+      >
+        {{paragraph}}
+      </p>
+    </GradientContainer>
+  </PageLayout>
 </template>
 
 <script>
+import {computed, ref, watchEffect} from 'vue'
+import {useI18n} from 'vue-i18n'
+import useParseText from '../hooks/useParseText'
+import commonApi from '../api/common'
+
+import PageLayout from '../components/Layout/PageLayout'
+import GradientContainer from '../components/Layout/GradientContainer'
+
 export default {
   name: 'RefundPolicy',
+  components: {GradientContainer, PageLayout},
+  setup() {
+    const {t, locale} = useI18n()
+    const pageData = ref({})
+    const paragraphs = computed(() => useParseText(pageData.value.meta_description || '').value)
+
+    watchEffect(async () => {
+      pageData.value = await commonApi.getRefundPolicy(locale.value)
+    })
+
+    return {
+      t,
+      paragraphs,
+    }
+  },
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "../assets/scss/variables";
+@import "../assets/scss/breakpoints";
+@import "../assets/scss/page-helpers";
 
+.refund {
+  @include tablets() {
+    overflow-y: auto;
+  }
+
+  @include mobile-landscape() {
+    height: calc(#{$page-height} + #{$spacing-lg});
+  }
+
+  @media screen and (min-width: 768px) and (min-height: 768px) {
+    @include page-height;
+  }
+}
 </style>
